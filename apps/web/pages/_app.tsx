68509dc22c22eb8e1126475256608d1d72583ab2
Nate Daines <nate@thedaines.org>
2022-04-29 18:02:41 +1000

    added react-query to web project

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import { AuthProvider } from "web/context/AuthContext";
+
+const queryClient = new QueryClient();
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
